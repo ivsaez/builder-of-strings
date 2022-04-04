a15d6de404c181fe5ff24ea0a2cccbb45f8c8298
Ivan Saez Zamora <ivsaez@gmail.com>
2022-04-04 20:46:26 +0200

    implement builder

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,64 @@
-function hello(name: string = "Ivan"): string {
-  return `Hello, ${name}`;
-}
+export type StringBuildable = string | number | boolean;
 
-export default hello;
+export class StringBuilder{
+  private _items: StringBuildable[];
+
+  constructor(input: StringBuildable = null){
+    this._items = [];
+
+    this.append(input);
+  }
+
+  append(input: StringBuildable): StringBuilder{
+    if(this.isValidInput(input)){
+      this._items.push(input);
+    }
+
+    return this;
+  }
+
+  appendLine(input:StringBuildable): StringBuilder{
+    this._items.push(StringBuilder.Line);
+    return this.append(input);
+  }
+
+  appendWhen(condition: () => boolean, input: StringBuildable): StringBuilder{
+    if(this.isValidCondition(condition)){
+      return this.append(input);
+    }
+
+    return this;
+  }
+
+  appendLineWhen(condition: () => boolean, input: StringBuildable): StringBuilder{
+    if(this.isValidCondition(condition)){
+      return this.appendLine(input);
+    }
+
+    return this;
+  }
+
+  clear(): void{
+    this._items = [];
+  }
+
+  toString(): string{
+    return this._items.length === 0
+      ? ""
+      : this._items.join("");
+  }
+
+  static get Line(): string{
+    return process.platform === "win32"
+      ? "\r\n"
+      : "\n";
+  }
+
+  private isValidInput(input: StringBuildable): boolean{
+    return input != null;
+  }
+
+  private isValidCondition(condition: () => boolean): boolean{
+    return condition != null && condition();
+  }
+}
--- src/tests/index.test.ts
+++ src/tests/index.test.ts
@@ -1,11 +1,57 @@
-import hello from "..";
+import { StringBuilder } from "../index";
 
-describe("it should say hello", () => {
-  it("should greet 'Ivan'", () => {
-    expect(hello()).toBe("Hello, Ivan");
+let builder: StringBuilder;
+
+describe("StringBuilder should", () => {
+  beforeEach(() =>{
+    builder = new StringBuilder();
   });
 
-  it("should greet 'Daniel'", () => {
-    expect(hello("Daniel")).toBe("Hello, Daniel");
+  it("return empty string if no inputs", () => {
+    expect(builder.toString()).toBe("");
+  });
+
+  it("append inputs in the same line", () => {
+    builder
+      .append("a")
+      .append(null)
+      .append(true)
+      .append(null);
+
+    expect(builder.toString()).toBe("atrue");
+  });
+
+  it("append inputs in different lines", () => {
+    builder
+      .appendLine("a")
+      .appendLine(null)
+      .appendLine('a')
+
+    expect(builder.toString()).toBe(`${StringBuilder.Line}a${StringBuilder.Line}${StringBuilder.Line}a`);
+  });
+
+  it("append inputs in the same line when condition is met", () => {
+    let trueCondition = true;
+    let falseCondition = false;
+
+    builder
+      .append("a")
+      .appendWhen(() => trueCondition, 5)
+      .append('a')
+      .appendWhen(() => falseCondition, "b");
+
+    expect(builder.toString()).toBe("a5a");
+  });
+
+  it("append inputs in different lines", () => {
+    let trueCondition = true;
+    let falseCondition = false;
+
+    builder
+      .appendLineWhen(() => trueCondition, "a")
+      .appendLineWhen(() => falseCondition, "b")
+      .appendLine('a')
+
+    expect(builder.toString()).toBe(`${StringBuilder.Line}a${StringBuilder.Line}a`);
   });
 });
